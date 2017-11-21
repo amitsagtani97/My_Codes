49426446a54a6919beb94deeebe328fe37ed3a23
Amit Sagtani <asagtani06@gmail.com>
2017-11-21 23:55:25 +0530

    c learning

--- DSA/Experimenting data types/1.c
+++ DSA/Experimenting data types/1.c
@@ -1,22 +1,5 @@
 #include<stdio.h>
-main( )
+int main()
 {
-int x, y, s = 2 ;
-s *= 3 ;
-y = f ( s ) ;
-x = g ( s ) ;
-printf ( "\n%d %d %d", s, y, x ) ;
-}
-int t = 8 ;
-f ( int a )
-{
-a += -5 ;
-t -= 4 ;
-return ( a + t ) ;
-}
-g ( int a )
-{
-a = 1 ;
-t += a ;
-return ( a + t ) ;
+    printf("Hello World!");
 }
